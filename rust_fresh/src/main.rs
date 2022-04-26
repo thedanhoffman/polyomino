396d788085a0faefe80e7fa31c94f61af7038a42
Daniel Hoffman <djh4@illinois.edu>
2022-04-26 18:23:26 +0000

    cleaned up the code a bit

--- rust_fresh/src/main.rs
+++ rust_fresh/src/main.rs
@@ -63,38 +63,16 @@ mod tests {
 
         #[test]
         fn test_explicit_trominoes() {
-            let mut compute = |n: u64| -> u64 {
-                a(n)
-            };
-
-            for i in (1..=10) {
-                println!("3x{}: {}", i, compute(i));
-            }
-
-            assert_eq![compute(1), 1];
-            assert_eq![compute(2), 3];
-            assert_eq![compute(3), 10];
-            assert_eq![compute(4), 23];
-            assert_eq![compute(5), 62];
-            assert_eq![compute(6), 170];
-            assert_eq![compute(7), 441];
-            assert_eq![compute(8), 1173];
-            assert_eq![compute(9), 3127];
-            assert_eq![compute(10), 8266];
-
-            // NOTE: "some states are not reachable within the first N rows, so the actual
-            // number of states involved in solving the NxN board was slightly lower).
-            // I don't believe this is relevant for the 3x3 case
-
-            // NOTE: does the 3x3 case have additional symmetries simplified away because
-            // of diagonal symmetries?
-        
-            // "above row one" ... "by the 9th row" makes me think it is evaluating
-            // the slice of the top row *from above* (meaning the slice is the boundary),
-            // so any implicit summations are out
-        
-            // NOTE: the area in the boxes in Figure 4 is the total amount of area currently
-            // drawn
+            assert_eq![a(1), 1];
+            assert_eq![a(2), 3];
+            assert_eq![a(3), 10];
+            assert_eq![a(4), 23];
+            assert_eq![a(5), 62];
+            assert_eq![a(6), 170];
+            assert_eq![a(7), 441];
+            assert_eq![a(8), 1173];
+            assert_eq![a(9), 3127];
+            assert_eq![a(10), 8266];
         }
     }
 }
